aadbe093af8264455041adfed34e2e6dd517d095
Felipe <Felipe@Felipe>
2016-08-25 21:26:45 -0500

    pojo creado a de el modulo de persistencia

---
+++ Lab_01/src/java/com/Lab_01/DAO/AutosFacadeLocal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Lab_01.DAO;
+
+import com.Lab_01.controller.Autos;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Felipe
+ */
+@Local
+public interface AutosFacadeLocal {
+
+    void create(Autos autos);
+
+    void edit(Autos autos);
+
+    void remove(Autos autos);
+
+    Autos find(Object id);
+
+    List<Autos> findAll();
+
+    List<Autos> findRange(int[] range);
+
+    int count();
+    
+}
---
+++ Lab_01/src/java/com/Lab_01/DAO/VentaFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Lab_01.DAO;
+
+import com.Lab_01.controller.Venta;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Felipe
+ */
+@Stateless
+public class VentaFacade extends AbstractFacade<Venta> implements VentaFacadeLocal {
+
+    @PersistenceContext(unitName = "Lab_01PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public VentaFacade() {
+        super(Venta.class);
+    }
+    
+}
